43b3afec1997b60872b20e6f4166fb1772bd638e
Gabriel Pequeno <gabriel.pequeno6@gmail.com>
2023-10-06 22:37:42 -0300

    criei classe Hero e classe DataContext

---
+++ dota2LobbyGenerator/dota2LobbyGenerator/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using dota2LobbyGenerator.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dota2LobbyGenerator.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }   
+        
+        public DbSet<Hero> Heroes { get; set; }     
+        
+    }
+}
---
+++ dota2LobbyGenerator/dota2LobbyGenerator/Entities/Hero.cs
@@ -0,0 +1,9 @@
+﻿namespace dota2LobbyGenerator.Entities
+{
+    public class Hero
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }   
+        public string Atribute { get; set; }
+    }
+}
