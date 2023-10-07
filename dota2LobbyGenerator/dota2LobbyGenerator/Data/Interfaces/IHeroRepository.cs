d041d64190027204907680d4b5b3e78ef212db5d
Gabriel Pequeno <gabriel.pequeno6@gmail.com>
2023-10-06 23:43:49 -0300

    criei crud na classe concreta de HeroRepository

---
+++ dota2LobbyGenerator/dota2LobbyGenerator/Data/Interfaces/IHeroRepository.cs
@@ -0,0 +1,14 @@
+﻿using dota2LobbyGenerator.Entities;
+
+namespace dota2LobbyGenerator.Data.Interfaces
+{
+    public interface IHeroRepository
+    {
+        //crud
+        Task Create(Hero hero);
+        Task<List<Hero>> GetAll();
+        Task<Hero> GetById(int id);
+        Task Update(Hero hero);
+        Task Delete(Hero hero);        
+    }
+}
---
+++ dota2LobbyGenerator/dota2LobbyGenerator/Data/Repos/HeroRepository.cs
@@ -0,0 +1,66 @@
+﻿using dota2LobbyGenerator.Data.Interfaces;
+using dota2LobbyGenerator.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dota2LobbyGenerator.Data.Repos
+{
+    public class HeroRepository : IHeroRepository
+    {
+        private readonly DataContext _dataContext;
+        public HeroRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;   
+        }
+        public async Task Create(Hero hero)
+        {
+            try
+            {
+                _dataContext.Heroes.Add(hero);
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task Delete(Hero hero)
+        {
+            try
+            {
+                _dataContext.Heroes.Remove(hero);
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<List<Hero>> GetAll()
+        {
+            return await _dataContext.Heroes.ToListAsync();
+        }
+
+        public Task<Hero> GetById(int id)
+        {
+            return _dataContext.Heroes.AsNoTracking().FirstOrDefaultAsync(h => h.Id == id);
+        }
+
+        public async Task Update(Hero hero)
+        {
+            try
+            {
+                _dataContext.Heroes.Update(hero);
+                await _dataContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
