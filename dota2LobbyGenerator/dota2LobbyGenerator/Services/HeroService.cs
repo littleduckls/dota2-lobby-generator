fb75558a347807be10e2fab35cd341d27d45ee32
Gabriel Pequeno <gabriel.pequeno6@gmail.com>
2023-10-10 20:17:03 -0300

    criei a interface e o service de hero

---
+++ dota2LobbyGenerator/dota2LobbyGenerator/Services/HeroService.cs
@@ -0,0 +1,41 @@
+﻿using dota2LobbyGenerator.Data.Interfaces;
+using dota2LobbyGenerator.Entities;
+using dota2LobbyGenerator.Services.Interfaces;
+
+namespace dota2LobbyGenerator.Services
+{
+    public class HeroService : IHeroService
+    {
+        private readonly IHeroRepository _heroRepository;
+        public HeroService(IHeroRepository heroRepository)
+        {
+            _heroRepository = heroRepository;      
+        }
+        public async Task CreateHero(Hero hero)
+        {
+            await _heroRepository.Create(hero); 
+        }
+
+        public async Task DeleteHero(Hero hero)
+        {
+            await _heroRepository.Delete(hero);
+        }
+
+        public async Task<List<Hero>> GetAllHeroes()
+        {
+            var heroes = await _heroRepository.GetAll();
+            return heroes;  
+        }
+
+        public async Task<Hero> GetHeroById(int id)
+        {
+            var hero = await _heroRepository.GetById(id);
+            return hero;    
+        }
+
+        public async Task UpdateHero(Hero hero)
+        {
+            await _heroRepository.Update(hero);   
+        }
+    }
+}
---
+++ dota2LobbyGenerator/dota2LobbyGenerator/Services/Interfaces/IHeroService.cs
@@ -0,0 +1,14 @@
+﻿using dota2LobbyGenerator.Entities;
+
+namespace dota2LobbyGenerator.Services.Interfaces
+{
+    public interface IHeroService
+    {
+        //crud
+        Task CreateHero(Hero hero);
+        Task<List<Hero>> GetAllHeroes();
+        Task<Hero> GetHeroById(int id);
+        Task UpdateHero(Hero hero);
+        Task DeleteHero(Hero hero);
+    }
+}
